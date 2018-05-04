728cfc3a791b3f6f03a9c7435ec87f6ccfaf7316
Kseniia Kozhukhova <seniurbano@gmail.com>
2018-05-04 17:25:33 +0300

    Create add task function

--- js/app.js
+++ js/app.js
@@ -4,3 +4,28 @@ const taskForm = document.querySelector('#task-form'),      taskField = document
       tasksFilter = document.querySelector('#filter'),
       tasksClearBtn = document.querySelector('.btn__clear-tasks');
 
+// Load all event listeners
+loadAllEventListeners();
+
+// Load all event listeners function
+function loadAllEventListeners() {
+  // Add task to the tasks list
+  taskForm.addEventListener('submit', addTask);
+
+
+};
+
+// Add task to the tasks list function
+function addTask(e) {
+  e.preventDefault();
+  // Check task field for its value
+  if (taskField.value === '') {
+    alert('Type a task');
+  };
+
+  // Create li element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+
+  console.log(li);
+}
